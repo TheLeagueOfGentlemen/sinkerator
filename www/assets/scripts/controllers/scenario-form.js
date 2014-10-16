ce67e669f5542e3af94bab8d999acd32dfe1e8d8
Ben Glassman <ben@vtdesignworks.com>
2014-10-16 17:49:24 -0400

    Add model for scenario form data.

--- www/assets/scripts/controllers/scenario-form.js
+++ www/assets/scripts/controllers/scenario-form.js
@@ -1,30 +1,28 @@
+var ScenarioFormData = require('./../models/scenario-form-data');
+
 module.exports = [
         '$state', '$scope', 'CustomerData', 'Scenario',
 function($state,   $scope,   CustomerData,   Scenario) {
-  $scope.name = 'Relaxo\'s House';
-  $scope.numBathrooms = 1;
-  $scope.numBedrooms = 1;
-  $scope.kitchenName = '';
-  $scope.basementName = '';
-  $scope.officeName = '';
-  $scope.livingRoomName = '';
+  $scope.FormData = new ScenarioFormData();
+  $scope.FormData.name = "Relaxo's House";
 
   $scope.bathroomOptions = [1,2,3,4];
   $scope.bedroomOptions = [1,2,3,4];
 
   $scope.submit = function() {
+    var FormData = $scope.FormData;
     CustomerData.kwh = 1000;
     CustomerData.cost = 10;
     Scenario.reset();
-    for (var i = 1; i <= $scope.numBathrooms; i++) {
+    for (var i = 1; i <= FormData.numBathrooms; i++) {
       Scenario.addRoom('Bathroom '+i);
     }
-    for (var j = 1; j <= $scope.numBedrooms; j++) {
+    for (var j = 1; j <= FormData.numBedrooms; j++) {
       Scenario.addRoom('Bedroom '+j);
     }
     var roomNames = ['kitchenName', 'basementName', 'officeName', 'livingRoomName'];
     for (var k = 0; k < roomNames.length; k++) {
-      var roomName = $scope[roomNames[k]];
+      var roomName = FormData[roomNames[k]];
       if (roomName) {
         Scenario.addRoom(roomName);
       }
---
+++ www/assets/scripts/models/scenario-form-data.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+  this.name = '';
+  this.numBedrooms = 1;
+  this.numBathrooms = 1;
+  this.kitchenName = '';
+  this.livingRoomName = '';
+  this.basementName = '';
+  this.officeName = '';
+};
