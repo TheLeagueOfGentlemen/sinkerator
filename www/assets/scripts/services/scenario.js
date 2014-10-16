940de75d938423c266674cfcaa8e8db62ba4c186
Ben Glassman <ben@vtdesignworks.com>
2014-10-16 19:46:23 -0400

    add fixme note.

--- www/assets/scripts/services/scenario.js
+++ www/assets/scripts/services/scenario.js
@@ -7,6 +7,7 @@ function(RoomColorDispenser) {
       this.rooms = [];
       this.colors.reset();
     },
+    // FIXME: This removeAppliance/addAppliance could shouldt be here, the room object should get passed in.
     addRoom: function(name) {
       this.rooms.push({
         name: name,
