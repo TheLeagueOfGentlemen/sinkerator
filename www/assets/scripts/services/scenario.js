29ad53502fbca742365db9193cd9e2c0e4afeb4b
Ben Glassman <ben@vtdesignworks.com>
2014-10-16 18:11:36 -0400

    Add example appliance when adding a room.

--- www/assets/scripts/services/scenario.js
+++ www/assets/scripts/services/scenario.js
@@ -10,7 +10,18 @@ function(RoomColorDispenser) {
     addRoom: function(name) {
       this.rooms.push({
         name: name,
-        color: this.colors.dispense()
+        color: this.colors.dispense(),
+        appliances: [
+          {
+            name: 'Air Conditioner',
+            wattage: 500,
+            hours_per_week: 20,
+            daily_usage: {
+              kwh: 4,
+              cost: 2.5
+            }
+          }
+        ]
       });
     },
     rooms: [],
